9fb9675c2b6e7c1d68b5d7548260b404a8b89cfc
marinaAC <maru.cardozo02@gmail.com>
2020-03-03 21:06:23 -0300

    el nueve resuelto

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,77 @@
 function mostrar()
 {
-    
+    var marca;
+    var peso;
+    var seguir;
+    var temperatura;
+    var contadorPares = 0;
+    var acumuladorPares = 0;
+    var contadoMasPesado = 0;
+    var acumuladorProductoMenosCero = 0;
+    var promedioPeso;
+    var pesoMaximo;
+    var pesoMinimo;
+    var flag = 0;
+    var resto;
+    var marcaMasPesada;
+    var cantidadProductos = 0;
+    var acumuladorPesos = 0;
+
+
+    do{
+        //Ingreso de datos y validacion de datos
+        marca = prompt("Ingrese una marca");
+        while(marca == null || marca == ""){
+            marca = prompt("Ingrese una marca");
+        }
+
+        peso = prompt("Ingrese el peso (debe estar entre 1 y 100)")
+        peso = parseInt(peso);
+        while(isNaN(peso)||peso<1 || peso > 100){
+            peso = prompt("Error! Ingrese el peso (debe estar entre 1 y 100)")
+            peso = parseInt(peso);
+        }
+
+        temperatura = prompt("Ingrese el peso (debe estar entre -30 y 30)")
+        temperatura = parseInt(temperatura);
+        while(isNaN(temperatura)||temperatura<-30 || temperatura > 30){
+            temperatura = prompt("Error! Ingrese el peso (debe estar entre -30 y 30)")
+            temperatura = parseInt(temperatura);
+        }
+
+        //evaluar las condiciones
+        resto = temperatura % 2;
+        if(resto == 0){
+            acumuladorPares = acumuladorPares + temperatura;
+            contadorPares++;
+        }
+
+        // pesos maximos y pesos minimos
+        if(flag == 0){
+            pesoMaximo = peso;
+            pesoMinimo = peso;
+            marcaMasPesada = marca;
+            flag = 1;
+        } else if(peso>pesoMaximo){
+            pesoMaximo = peso;
+            marcaMasPesada = marca;
+        } else if(peso<pesoMinimo){
+            pesoMinimo = peso;
+        }
+
+        if(temperatura < 0){
+            acumuladorProductoMenosCero++;
+        }
+
+        acumuladorPesos += peso;
+        cantidadProductos++;
+        seguir=confirm("Desea Seguir?");
+    }while(seguir);
+
+
+    if(cantidadProductos>0){
+        promedioPeso = acumuladorPesos / cantidadProductos;
+    }
+
+    document.write("")
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/once.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/once.js
@@ -1,4 +1,42 @@
 function mostrar()
 {
     
+   // var continuar=true;
+    /*while(continuar){
+        continuar= confirm("Usted desea seguir cargando datos?");
+    }*/
+    /*
+    var continuar;
+    do{
+        continuar= confirm("Usted desea seguir cargando datos?");
+    }while(continuar);*/
+
+    var respuesta = "si";
+    while(respuesta=="si"){
+
+            //pedir los datos
+            //validar los datos
+
+            //calcular 
+
+            var formasDePago = prompt("ingrese una forma de pago");
+            formasDePago = formasDePago.toLocaleLowerCase();
+            while(formasDePago.toLocaleLowerCase() != "efectivo" && formasDePago.toLocaleLowerCase() != "qr"){
+                
+            }
+
+
+            formasDePago.toLocaleLowerCase()
+
+
+
+
+        respuesta=prompt("Si desea continuar escriba si");
+    }
+/*
+    var respuesta;
+    do{
+        respuesta=prompt("Si desea continuar escriba si"); 
+    }while(respuesta == "si");*/
+    
 }
